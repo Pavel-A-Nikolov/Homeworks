11cadeb47d78c819682e741ee7fcee6078ff1579
Pavel-A-Nikolov <43548257+Pavel-A-Nikolov@users.noreply.github.com>
2018-09-30 17:43:55 +0300

    d

---
+++ Domrabota/okrazhnost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace okrazhnost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius - r:");
+            double r = double.Parse(Console.ReadLine());
+            Console.Write("The Area=pi*r^2=3,14*r*r=");
+            Console.WriteLine(3.14 * r * r);
+            Console.Write("The Curcumference=pi*2*r=6,28*r=");
+            Console.WriteLine(r * 6.28);
+        }
+    }
+}
---
+++ Domrabota/triagalnik/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triagalnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first side - a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the other side - b:");
+            double h = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the third side - c:");
+            double c = double.Parse(Console.ReadLine());
+            Console.Write("The perimeter - P=a+b+c=");
+            Console.WriteLine(a + h + c);
+            Console.WriteLine("Now enter only one side - x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("And the coresponding height - h:");
+            double g = double.Parse(Console.ReadLine());
+            Console.Write("The face - S=0,5*x*h=");
+            Console.WriteLine(0.5 * x * g);
+        }
+    }
+}
